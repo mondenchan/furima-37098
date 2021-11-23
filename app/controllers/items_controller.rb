b7a5213d26ea44f3c358302ae111ad9a03cca024
mondenchan <monden2016@gmail.com>
2021-11-23 12:13:59 +0900

    前項目でのレビュー任意修正分を反映

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,10 +23,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user_id == current_user.id
-    else
-      redirect_to root_path
-    end
+    return redirect_to root_path if @item.user_id != current_user.id
   end
 
   def update
